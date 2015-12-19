692ceb97025103275c633d7259740be285501f84
laoliang <andrew@rokid.com>
2015-12-19 17:45:08 +0800

    first

---
+++ ScanTest/app/src/main/java/rokid/scantest/MainActivity.java
@@ -0,0 +1,19 @@
+package rokid.scantest;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.zxing.client.android.CaptureActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(this, CaptureActivity.class);
+        startActivity(intent);
+    }
+}
